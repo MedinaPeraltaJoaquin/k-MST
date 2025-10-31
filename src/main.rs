55f782d8241c5df30c6232b5c9ac96f06f82e316
MedinaPeraltaJoaquin <joaquin.medina513@ciencias.unam.mx>
2025-10-31 14:22:02 -0600

    feature: creación de heuristica y archivo main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 mod utils;
+mod entity;
+mod woa;
 
 use std::{env, process::exit};
 
 use utils::read_input::ReadInput;
+use utils::config::Config;
+use utils::svg_plot::plot_convergence;
+use utils::svg_tree_plot::plot_tree;
+use entity::graph::Graph;
+use woa::woa::WOA;
+
 
 pub fn main(){
     let args : Vec<String> = env::args().collect();
@@ -18,6 +26,7 @@ pub fn main(){
         exit(0);
     }
 
+    println!("Iniciando k-MST con Whale Optimization Algorithm (WOA)");
     let verbose_mode = read_input.get_verbose();
     let svg_mode = read_input.get_svg();
 
@@ -35,16 +44,65 @@ pub fn main(){
         }
     };
 
-    let mut graph_vec = match read_input.read_file() {
+    println!("Cargando el grafo desde el archivo...");
+    let graph_vec = match read_input.read_file() {
         Ok(result) => result,
         Err(e) => {
             panic!("{:?}",e);
         }
     };
 
-    println!("k: {}",k_nodes);
-    println!("graph: {:?}",graph_vec);
-    println!("seeds: {:?}",seeds);
+    let graph = Graph::new(graph_vec,k_nodes);
+    
+    let config = Config::from_env();
 
+    let mut best_solution = std::f64::INFINITY;
+    let mut best_seed = seeds[0];
+    for seed in &seeds {
+        println!("Running WOA with seed: {}", seed);
+
+        let mut woa = WOA::new(
+            config.size_population,
+            config.max_iteration,
+            config.lb,
+            config.ub,
+            *seed as u64,
+            k_nodes,
+            &graph
+        );
+
+        woa.woa(&graph);
+        let mut best_whale = woa.get_best_whale();
+        let convergence = woa.get_convergence();
+        let idx_best_whale = woa.get_idx_best_whale();
+        println!("Seed: {}: Best Cost: {}",seed,best_whale.get_cost(&graph));
+        if !best_whale.get_tree_struct().is_connected(&graph) {
+            panic!("Warning: The best whale's tree is not connected or inconsistent!");
+        }
+        if verbose_mode {
+            println!("Best Whale Index in Population: {}", idx_best_whale);
+            println!("Best Whale Cost: {}", best_whale.get_cost(&graph));
+        }
+        if svg_mode {
+            let filename_plot = format!("./svg/convergence_seed_{}.svg",seed);
+            match plot_convergence(&convergence, &filename_plot) {
+                Ok(_) => println!("Gráfica de convergencia guardada en: {}", filename_plot),
+                Err(e) => eprintln!("Error al guardar la gráfica de convergencia: {}", e),
+            };
+
+            let filename_tree = format!("./svg/tree_seed_{}.svg",seed);
+            match plot_tree(best_whale.get_tree(), &filename_tree) {
+                Ok(_) => println!("Árbol guardado en: {}", filename_tree),
+                Err(e) => eprintln!("Error al guardar el árbol: {}", e),
+            };
+        }
+
+        if best_whale.get_cost(&graph) < best_solution {
+            best_solution = best_whale.get_cost(&graph);
+            best_seed = *seed;
+        }
+    }
+
+    println!("Mejor solución encontrada con semilla {}: Costo = {}", best_seed, best_solution);
     exit(0);
 }
---
+++ src/woa/whale.rs
@@ -0,0 +1,115 @@
+use core::f64;
+
+use rand::{rngs::StdRng, Rng};
+use svg::node;
+
+use crate::entity::{graph::Graph, tree::Tree};
+
+fn calculate_value(x : f64) -> f64{
+    1.0 / (1.0 + (-10.0 * (x - 0.5)).exp())
+}
+
+#[derive(Debug,Clone)]
+pub struct Whale {
+    position : Vec<f64>,
+    nodes : Vec<(String,bool)>,
+    pub tree : Tree,
+    pub cost : f64,
+    size: usize,
+    pub lb : f64,
+    pub ub : f64
+} 
+
+impl Whale {
+    pub fn new(graph : &Graph, lb : f64 , ub : f64, random : &mut StdRng, k : usize) -> Self {
+        let size = graph.get_num_nodes();
+        let mut position = vec![lb + random.gen_range(0.0..=1.0) * (ub -lb);size];
+        let nodes = graph.get_nodes();
+        let mut nodes_tree_ref : Vec<(String,bool)> = nodes.iter().map(|n| (n.clone(), false)).collect();
+        let mut nodes_tree: Vec<(String, bool)> = vec![];
+        let mut k_element = 1;
+        while k_element <= k {
+            let i = random.gen_range(0..size);
+            if nodes_tree_ref[i].1 {
+                continue;
+            }
+
+            let limit = random.gen_range(0.0..1.0);
+            nodes_tree_ref[i].1 = calculate_value(position[i]) > limit;
+            if nodes_tree_ref[i].1 {
+                nodes_tree.push((nodes_tree_ref[i].0.clone(), false));
+                k_element +=1;
+            } else {
+                position[i] = lb + random.gen_range(0.0..=1.0) * (ub -lb)
+            }
+        }
+
+        let mut tree = graph.generate_tree_by_nodes(k, &mut nodes_tree);
+        let cost = tree.get_cost(graph);
+
+        Self {
+            position : position,
+            nodes : nodes_tree_ref.clone(),
+            tree : tree.clone(),
+            cost,
+            size : size,
+            lb,
+            ub
+        }
+    }
+
+    pub fn get_cost(&mut self, graph : &Graph) -> f64 {
+        self.tree.get_cost(graph)
+    }
+
+    pub fn get_tree(&self) -> Vec<(String, String, f64)> {
+        self.tree.get_edges()
+    }
+
+    pub fn get_tree_struct(&self) -> Tree {
+        self.tree.clone()
+    }
+
+    pub fn get_len_position(&self) -> usize {
+        self.size
+    }
+
+    pub fn get_position(&self, index : usize) -> f64 {
+        self.position[index]
+    } 
+
+    pub fn get_positions(&self) -> Vec<f64> {
+        self.position.clone()
+    }
+
+    pub fn get_node(&self, index : usize) -> (String,bool) {
+        self.nodes[index].clone()
+    }
+
+    pub fn set_position(&mut self, index : usize, value : f64) {
+        self.position[index] = value
+    }
+
+    pub fn set_node(&mut self, index : usize, in_tree : bool) {
+        self.nodes[index].1 = in_tree
+    }
+
+    pub fn get_index_node_in_tree(&self, random : &mut StdRng) -> usize {
+        let mut index = random.gen_range(0..self.size);
+        if self.nodes.iter().all(|(_, in_tree)| !in_tree) { return index; } 
+        while self.nodes[index].1 {
+            index = random.gen_range(0..self.size);
+        }
+        index
+    }
+
+    pub fn get_index_node_nin_tree(&self, random : &mut StdRng) -> usize {
+        let mut index = random.gen_range(0..self.size);
+        if self.nodes.iter().all(|(_, in_tree)| *in_tree) { return index; } 
+        while !self.nodes[index].1 {
+            index = random.gen_range(0..self.size);
+        }
+        index
+    }
+
+}
